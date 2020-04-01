6454a64ba19905ffceafc38856a2deca6e385f17
Maxwell Handler <mhandler1991@gmail.com>
2020-03-31 23:55:19 -0700

    Full Tab Handeling

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,18 +9,31 @@ function tabUpdate(id){
 
     switch(id) {
         case 'tab1':
+
             console.log('Switch Result: Tab1');
-            document.getElementById('tab1').classList.add('slds-is-active');
+
+            // Tab Handeling...
+            // Remove Classes - Tabs
             document.getElementById('tab2').classList.remove('slds-is-active');
             document.getElementById('tab3').classList.remove('slds-is-active');
-            document.getElementById('tab1content').classList.add('slds-show');
+            // Add Classes - Tabs
+            document.getElementById('tab1').classList.add('slds-is-active');
+            // Remove Classes - Content
             document.getElementById('tab2content').classList.remove('slds-show');
-            document.getElementById('tab2content').classList.add('slds-hide');
             document.getElementById('tab3content').classList.remove('slds-show');
+            document.getElementById('tab1content').classList.remove('slds-hide');
+            // Add Classes - Content
+            document.getElementById('tab1content').classList.add('slds-show');
+            document.getElementById('tab2content').classList.add('slds-hide');
             document.getElementById('tab3content').classList.add('slds-hide');
+            // End Tab Handeling...
+            console.log('Boy, we handeled those tabs...');
+
             break;
         case 'tab2':
+
             console.log('Switch Result: Tab2');
+
             // Tab Handeling...
             // Remove Classes - Tabs
             document.getElementById('tab1').classList.remove('slds-is-active');
@@ -36,9 +49,30 @@ function tabUpdate(id){
             document.getElementById('tab1content').classList.add('slds-hide');
             document.getElementById('tab3content').classList.add('slds-hide');
             // End Tab Handeling...
+            console.log('Boy, we handeled those tabs...');
+
             break;
         case 'tab3':
+
             console.log('Switch Result: Tab3');
+
+            // Tab Handeling...
+            // Remove Classes - Tabs
+            document.getElementById('tab1').classList.remove('slds-is-active');
+            document.getElementById('tab2').classList.remove('slds-is-active');
+            // Add Classes - Tabs
+            document.getElementById('tab3').classList.add('slds-is-active');
+            // Remove Classes - Content
+            document.getElementById('tab1content').classList.remove('slds-show');
+            document.getElementById('tab2content').classList.remove('slds-show');
+            document.getElementById('tab3content').classList.remove('slds-hide');
+            // Add Classes - Content
+            document.getElementById('tab3content').classList.add('slds-show');
+            document.getElementById('tab1content').classList.add('slds-hide');
+            document.getElementById('tab2content').classList.add('slds-hide');
+            // End Tab Handeling...
+            console.log('Boy, we handeled those tabs...');
+
             break;
         default:
     }

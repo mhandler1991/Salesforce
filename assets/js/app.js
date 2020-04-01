2569ab2a067f45a41e3b04dcbc0a6f3b7f2bb44f
Maxwell Handler <mhandler1991@gmail.com>
2020-03-31 20:15:56 -0700

    remove class logging

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,7 @@ function tabUpdate(id){
     console.log("Tab Clicked!");
     console.log(id);
     document.getElementById('tab1').classList.remove('slds-is-active');
+    console.log('Removed Class...');
     document.getElementById(id).classList.add('slds-is-active');
     console.log('Updated Class...');
 }
